d54a0c805e370cfcebe658abe55065fa1908b9fc
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-30 12:07:11 +0200

    Rename User Repository Interface

--- src/main/java/com/debuggers/repository/IUserRepository.java
+++ src/main/java/com/debuggers/repository/IUserRepository.java
@@ -3,7 +3,7 @@ package com.debuggers.repository;
 import com.debuggers.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface IUserRepository extends JpaRepository<User,Long> {
     public User create();
     public User read(Long id);
     public User readAll();
