1211a93dcb2b7acb0218585636caee30ae00afe6
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-30 11:47:42 +0200

    Create Message Factory

--- src/main/java/com/debuggers/domain/Message.java
+++ src/main/java/com/debuggers/domain/Message.java
@@ -73,7 +73,7 @@ public class Message {
             return this;
         }
 
-        public Message builder(){
+        public Message build(){
             return new  Message(this);
         }
     }
