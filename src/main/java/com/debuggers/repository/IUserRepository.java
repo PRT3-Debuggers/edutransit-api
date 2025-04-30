9ae0d160b5ed378446b792cea3a1d9de0076af69
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-30 13:21:43 +0200

    Edit User Repository

--- src/main/java/com/debuggers/repository/IUserRepository.java
+++ src/main/java/com/debuggers/repository/IUserRepository.java
@@ -4,9 +4,5 @@ import com.debuggers.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<User,Long> {
-    public User create();
-    public User read(Long id);
-    public User readAll();
-    public void delete(User user);
-    public User update(User user);
+
 }
